944f3cf997140f8d133bbefd3afd94913ce2d067
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2025-01-17 16:22:04 +0100

    ADD: Hero choice && Fight View

--- process/choice-hero-process.php
+++ process/choice-hero-process.php
@@ -2,5 +2,26 @@
 
 require_once '../utils/autoloader.php';
 
-
 // TODO Use validators !!
+$validator = new ValidatorService();
+
+$validator->checkMethods('POST');
+
+$validator->addStrategy('hero_id', new RequiredValidator());
+$validator->addStrategy('hero_id', new IntegerValidator());
+
+if (!$validator->validate($_POST)) {
+    header('Location: ../public/choice-hero.php');
+    return;
+}
+
+$sanitizedData = $validator->sanitize($_POST);
+
+$heroRepository = new HeroRepository();
+$hero = $heroRepository->findById($sanitizedData['hero_id']);
+
+session_start();
+
+$_SESSION['hero'] = $hero;
+
+header("Location: /public/fight.php");
--- public/fight.php
+++ public/fight.php
@@ -2,28 +2,55 @@
 
 require_once '../utils/autoloader.php';
 
-if (!isset($_POST['hero_id'])) {
-    header("Location: ./choice-hero.php");
+session_start();
+
+if (!isset($_SESSION['hero'])) {
+    header('Location: /public/choice-hero.php');
     exit;
 }
 
-$heroRepository = new HeroRepository();
-$hero = $heroRepository->findById($_POST['hero_id']);
-
-if (!$hero) {
-    header("Location: ./choice-hero.php");
-    exit;
-}
+/**
+ * @var Hero $hero
+ */
+$hero = $_SESSION['hero'];
+$monster = new Monster(0, 'Monster', 80);
 
 require_once './partials/header.php';
 
-
 ?>
 
-<main class="">
+<main class="container mx-auto p-4 flex justify-center items-center min-h-screen">
+    <div class="text-center mb-8 w-full">
+        <h1 class="text-3xl font-bold text-white mb-8">Combat</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <article class="bg-gray-800 shadow-md rounded-lg overflow-hidden transform transition duration-500 p-4 relative">
+                <img src="<?php echo htmlspecialchars($hero->getPicturePath()); ?>" alt="<?php echo htmlspecialchars($hero->getName()); ?>" class="w-full h-48 object-contain mb-4">
+                <div class="p-4">
+                    <h2 class="text-xl font-bold mb-4 text-white">🦸 <?php echo htmlspecialchars($hero->getName()); ?></h2>
+                    <p class="text-gray-400 mb-4">❤️ Health: <?php echo htmlspecialchars($hero->getHealth()); ?>/100</p>
+                    <div class="w-full bg-gray-700 rounded-full h-2.5 mb-8">
+                        <div class="bg-green-500 h-2.5 rounded-full" style="width: <?php echo htmlspecialchars($hero->getHealth()); ?>%"></div>
+                    </div>
+                </div>
+            </article>
+            <article class="bg-gray-800 shadow-md rounded-lg overflow-hidden transform transition duration-500 p-4 relative">
+                <img src="/public/assets/imgs/goblin.gif" alt="<?php echo htmlspecialchars($monster->getName()); ?>" class="w-full h-48 object-contain mb-4">
+                <div class="p-4">
+                    <h2 class="text-xl font-bold mb-4 text-white">👾 <?= htmlspecialchars($monster->getName()); ?></h2>
+                    <p class="text-gray-400 mb-4">❤️ Health: <?= htmlspecialchars($monster->getHealth()); ?>/<?= htmlspecialchars($monster->getHealth()); ?></p>
+                    <div class="w-full bg-gray-700 rounded-full h-2.5 mb-8">
+                        <div class="bg-red-500 h-2.5 rounded-full" style="width: 100%"></div>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="text-center mt-8">
 
+            <button id="attack" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300">Attack</button>
+
+        </div>
+    </div>
 </main>
-
 
 <?php
 
